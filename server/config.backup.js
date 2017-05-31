b4c90bfa3dfd792296cb114ee37c4e0a9e818dcd
Brack Carmony <brackcarmony@gmail.com>
2017-05-31 16:16:55 -0400

    Removing config and adding example config.

--- server/config.backup.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  PORT: 8076,
-  MONGO_URI: 'mongodb://studentpair:8872B81CBF4D29C71D372CE88AD2C00E912E183FA3BD93A4A9DB69AF2BFB2956@localhost:27017/studentPair',
-  SESSION_SECRET: 'gweriwrb-erfawrg45-oasWsd', // Change your secret
-};
